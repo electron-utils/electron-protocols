05b2b9621308ae8da3691af1f649a2599068d956
Johnny Wu <KarasAya@gmail.com>
2017-04-19 12:04:16 +0800

    fix singleton typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const platform = require('electron-platform');
 const pkgJson = require('./package.json');
 
 let protocols;
-let name = `__electron_protocol__`;
+let name = `__electron_protocols__`;
 let msg = `Failed to require ${pkgJson.name}@${pkgJson.version}:
   A different version of ${pkgJson.name} already running in the process, we will redirect to it.
   Please make sure your dependencies use the same version of ${pkgJson.name}.`;
