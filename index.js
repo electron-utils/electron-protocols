431a78410dba26e4f7aa688fe09e83255121fcbf
Johnny Wu <KarasAya@gmail.com>
2017-04-19 10:55:14 +0800

    make sure we are singleton

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 'use strict';
 
-'use strict';
-
 const platform = require('electron-platform');
+const pkgJson = require('./package.json');
 
 let protocols;
+let name = `__electron_protocol__`;
+let msg = `Failed to require ${pkgJson.name}@${pkgJson.version}:
+  A different version of ${pkgJson.name} already running in the process, we will redirect to it.
+  Please make sure your dependencies use the same version of ${pkgJson.name}.`;
 
 if ( platform.isMainProcess ) {
-  protocols = require('./lib/protocols-main');
+  if (global[name]) {
+    console.warn(msg);
+    protocols = global[name];
+  } else {
+    protocols = require('./lib/protocols-main');
+  }
 } else {
-  protocols = require('./lib/protocols-renderer');
+  if (window[name]) {
+    console.warn(msg);
+    protocols = window[name];
+  } else {
+    protocols = require('./lib/protocols-renderer');
+  }
 }
 
 // ==========================
